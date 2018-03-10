95861fd4aa1b666443338228438f422744f45fb3
Andre Gugliotti <andre.nobrega@triboo.it>
2018-03-10 16:53:12 +0100

    Changes Installer
    Moves story table from installer to upgrader (for better learning)

--- app/code/local/Gugliotti/News/sql/gugliotti_news_setup/install-0.1.0.php
+++ app/code/local/Gugliotti/News/sql/gugliotti_news_setup/install-0.1.0.php
@@ -9,8 +9,7 @@
  * Gugliotti News SQL Installer.
  * @author Andre Gugliotti <andre@gugliotti.com.br>
  * @version 0.1.0
- * @category Training Modules
- * @package Gugliotti News
+ * @package Training Modules
  * @license GNU General Public License, version 3
  */
 
@@ -63,77 +62,9 @@ try {
     Mage::logException($e);
 }
 
-// prepare table for gugliotti_news_story
-$tableStory = $installer->getConnection()->newTable($installer->getTable('gugliotti_news/story'));
-
-try {
-    $tableStory->addColumn(
-        'story_id',
-        Varien_Db_Ddl_Table::TYPE_INTEGER,
-        null,
-        array('nullable' => false, 'identity' => true, 'primary' => true),
-        'News Story ID'
-    )->addColumn(
-        'title',
-        Varien_Db_Ddl_Table::TYPE_TEXT,
-        128,
-        array('nullable' => false),
-        'News Story Title'
-    )->addColumn(
-        'thumbnail_path',
-        Varien_Db_Ddl_Table::TYPE_TEXT,
-        128,
-        array('nullable' => true),
-        'News Story Thumbnail Path'
-    )->addColumn(
-        'content',
-        Varien_Db_Ddl_Table::TYPE_TEXT,
-        null,
-        array('nullable' => false),
-        'News Story Content'
-    )->addColumn(
-        'status',
-        Varien_Db_Ddl_Table::TYPE_BOOLEAN,
-        null,
-        array('nullable' => false, 'default' => 0),
-        'News Category Status'
-    )->addColumn(
-        'category_id',
-        Varien_Db_Ddl_Table::TYPE_INTEGER,
-        null,
-        array('nullable' => true),
-        'News Story Category'
-    )->addColumn(
-        'created_at',
-        Varien_Db_Ddl_Table::TYPE_TIMESTAMP,
-        null,
-        array('default' => Varien_Db_Ddl_Table::TIMESTAMP_INIT),
-        'News Story Created At'
-    )->addColumn(
-        'updated_at',
-        Varien_Db_Ddl_Table::TYPE_TIMESTAMP,
-        null,
-        array('default' => Varien_Db_Ddl_Table::TIMESTAMP_INIT_UPDATE),
-        'News Story Updated At'
-    )->addForeignKey(
-        $installer->getFkName(
-            'gugliotti_news/story',
-            'category_id',
-            'gugliotti_news/category',
-            'category_id'
-        ),
-        'category_id',
-        $installer->getTable('gugliotti_news/category'),
-        'category_id'
-    )->setComment('Gugliotti News Stories');
-} catch (Exception $e) {
-    Mage::logException($e);
-}
-
-// create tables
+// create table
 try {
     $installer->getConnection()->createTable($tableCategory);
-    $installer->getConnection()->createTable($tableStory);
 } catch (Exception $e) {
     Mage::logException($e);
 }
